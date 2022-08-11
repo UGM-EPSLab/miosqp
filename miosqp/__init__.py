264b5794ca2c703a3bdd56356940bda3fd19f7c9
Muhammad Yasirroni <48709672+yasirroni@users.noreply.github.com>
2022-08-11 23:37:02 +0700

    fix init on default_settings

--- miosqp/__init__.py
+++ miosqp/__init__.py
@@ -1,6 +1,6 @@
 from .constants import *  # noqa: F401
 from .data import add_bounds  # noqa: F401
-from .default_settings import get_miosqp_settings, get_osqp_settings  # noqa: F401
+from .default_settings import MIOSQP_SETTINGS, OSQP_SETTINGS  # noqa: F401
 from .solver import MIOSQP  # noqa: F401
 
 # This (EPSLab/epslab/utils/miosqp) is based on
